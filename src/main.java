00e24b3dda5124f05a080d665e225c216eb4e442
Anton Dragoman <dragomananton@gmail.com>
2020-11-12 10:46:44 -0500

    additional changes

--- src/main.java
+++ src/main.java
@@ -2,5 +2,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("hello last day git");
+        System.out.println("we are dedicated");
+        System.out.println("and determined");
     }
 }
